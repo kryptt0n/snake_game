a95514a9d11f0201f68d1490a44e3beafc829b5a
kryptt0n <vitaliksuhinin99@gmail.com>
2022-11-05 18:42:13 +0300

    added constructor for snake added method body for Room.java

--- src/Room.java
+++ src/Room.java
@@ -1,5 +1,50 @@
 public class Room {
+
+    private int width;
+    private int height;
+
+    private Snake snake;
+    private Mouse mouse;
+
     public static void main(String[] args) {
 
+    }
+
+    public Room(int width, int height, Snake snake) {
+        this.width = width;
+        this.height = height;
+        this.snake = snake;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
     }
 }
--- src/Snake.java
+++ src/Snake.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
     private List<SnakeSection> sections;
     private boolean isAlive;
     private SnakeDirection direction;
+
+    public Snake(int x, int y) {
+        sections = new ArrayList<>();
+        sections.add(new SnakeSection(x,y));
+        isAlive = true;
+    }
+
+    public int getX() {
+        return sections.get(0).getX();
+    }
+
+    public int getY() {
+        return sections.get(0).getY();
+    }
+
+    public void move() {
+        
+    }
 
     public void setDirection(SnakeDirection direction) {
         this.direction = direction;
